434ae42dbfb679885ccb6e7a7990e0b8850b6435
zhy75 <76135507+zhy75@users.noreply.github.com>
2023-04-24 12:06:22 -0400

    Update smartcontract.go

--- asset-transfer-basic/chaincode-go/chaincode/smartcontract.go
+++ asset-transfer-basic/chaincode-go/chaincode/smartcontract.go
@@ -15,23 +15,43 @@ type SmartContract struct {
 // Asset describes basic details of what makes up a simple asset
 // Insert struct field in alphabetic order => to achieve determinism across languages
 // golang keeps the order when marshal to json but doesn't order automatically
+// type Asset struct {
+// 	AppraisedValue int    `json:"AppraisedValue"`
+// 	Color          string `json:"Color"`
+// 	ID             string `json:"ID"`
+// 	Owner          string `json:"Owner"`
+// 	Size           int    `json:"Size"`
+// }
+
 type Asset struct {
-	AppraisedValue int    `json:"AppraisedValue"`
-	Color          string `json:"Color"`
-	ID             string `json:"ID"`
-	Owner          string `json:"Owner"`
-	Size           int    `json:"Size"`
+	Title        string `json:"Title"`
+	Date         string `json:"Date"`
+	Description  string `json:"Description"`
+	Grade        int    `json:"Grade"`
+	ID           string `json:"ID"`
+	InstructorID string `json:"InstructorID"`
+	Work         string `json:"Work"`
+	Owner        string `json:"Owner"`
 }
 
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	// assets := []Asset{
+	// 	{ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
+	// 	{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
+	// 	{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
+	// 	{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
+	// 	{ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
+	// 	{ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
+	// }
+
 	assets := []Asset{
-		{ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
-		{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
-		{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
-		{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
-		{ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
-		{ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
+		{ID: "student1", Title: "Title1", Date: "4/24/2023", Description: "Description 1", Grade: 0, Owner: "Instructor", Work: "", InstructorID: "Instructor"},
+		{ID: "student2", Title: "Title1", Date: "4/24/2023", Description: "Description 1", Grade: 0, Owner: "Instructor", Work: "", InstructorID: "Instructor"},
+		{ID: "student3", Title: "Title1", Date: "4/24/2023", Description: "Description 1", Grade: 0, Owner: "Instructor", Work: "", InstructorID: "Instructor"},
+		{ID: "student4", Title: "Title1", Date: "4/24/2023", Description: "Description 1", Grade: 0, Owner: "Instructor", Work: "", InstructorID: "Instructor"},
+		{ID: "student5", Title: "Title1", Date: "4/24/2023", Description: "Description 1", Grade: 0, Owner: "Instructor", Work: "", InstructorID: "Instructor"},
+		{ID: "student6", Title: "Title1", Date: "4/24/2023", Description: "Description 1", Grade: 0, Owner: "Instructor", Work: "", InstructorID: "Instructor"},
 	}
 
 	for _, asset := range assets {
@@ -50,7 +70,7 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 // CreateAsset issues a new asset to the world state with given details.
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, title string, grade int, owner string, date string, description string) error {
 	exists, err := s.AssetExists(ctx, id)
 	if err != nil {
 		return err
@@ -59,13 +79,25 @@ func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface,
 		return fmt.Errorf("the asset %s already exists", id)
 	}
 
+	// asset := Asset{
+	// 	ID:             id,
+	// 	Color:          color,
+	// 	Size:           size,
+	// 	Owner:          owner,
+	// 	AppraisedValue: appraisedValue,
+	// }
+
 	asset := Asset{
-		ID:             id,
-		Color:          color,
-		Size:           size,
-		Owner:          owner,
-		AppraisedValue: appraisedValue,
+		ID:           id,
+		Title:        title,
+		Date:         date,
+		Description:  description,
+		InstructorID: owner,
+		Grade:        grade,
+		Work:         "",
+		Owner:        owner,
 	}
+
 	assetJSON, err := json.Marshal(asset)
 	if err != nil {
 		return err
@@ -94,7 +126,7 @@ func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, i
 }
 
 // UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
+func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, title string, grade int, owner string, date string, description string) error {
 	exists, err := s.AssetExists(ctx, id)
 	if err != nil {
 		return err
@@ -104,12 +136,22 @@ func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface,
 	}
 
 	// overwriting original asset with new asset
+	// asset := Asset{
+	// 	ID:             id,
+	// 	Color:          color,
+	// 	Size:           size,
+	// 	Owner:          owner,
+	// 	AppraisedValue: appraisedValue,
+	// }
 	asset := Asset{
-		ID:             id,
-		Color:          color,
-		Size:           size,
-		Owner:          owner,
-		AppraisedValue: appraisedValue,
+		ID:           id,
+		Title:        title,
+		Date:         date,
+		Description:  description,
+		Work:         "",
+		InstructorID: "",
+		Grade:        grade,
+		Owner:        owner,
 	}
 	assetJSON, err := json.Marshal(asset)
 	if err != nil {
@@ -165,6 +207,52 @@ func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterfac
 	return oldOwner, nil
 }
 
+// TransferAsset updates the owner field of asset with given id in world state, and returns the old owner.
+func (s *SmartContract) GradeAssignment(ctx contractapi.TransactionContextInterface, id string, grade int) (int, error) {
+	asset, err := s.ReadAsset(ctx, id)
+	if err != nil {
+		return -1, err
+	}
+
+	oldGrade := asset.Grade
+	asset.Grade = grade
+
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return -1, err
+	}
+
+	err = ctx.GetStub().PutState(id, assetJSON)
+	if err != nil {
+		return -1, err
+	}
+
+	return oldGrade, nil
+}
+
+// TransferAsset updates the owner field of asset with given id in world state, and returns the old owner.
+func (s *SmartContract) SubmitAssignment(ctx contractapi.TransactionContextInterface, id string, work string) error {
+	asset, err := s.ReadAsset(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	// oldGrade := asset.Grade
+	asset.Work = work
+
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(id, assetJSON)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // GetAllAssets returns all assets found in world state
 func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
 	// range query with empty string for startKey and endKey does an
@@ -188,6 +276,36 @@ func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface
 			return nil, err
 		}
 		assets = append(assets, &asset)
+	}
+
+	return assets, nil
+}
+
+// GetAllAssets returns all assets found in world state
+func (s *SmartContract) GetAllAssignments(ctx contractapi.TransactionContextInterface, username string) ([]*Asset, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var assets []*Asset
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var asset Asset
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+		if asset.Owner[len(asset.Owner)-len(username):] == username {
+			assets = append(assets, &asset)
+		}
 	}
 
 	return assets, nil
